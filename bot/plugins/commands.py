b25b682f9658ffc6b44fe1f2b9e113cf3dba8305
joby-123 <85913522+joby-123@users.noreply.github.com>
2021-07-01 10:37:19 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -69,14 +69,14 @@ async def start(bot, update):
             await bot.send_audio(
                 chat_id=update.chat.id,
                 audio = file_id,
-                caption = caption,
+                caption = @latestmalayalam,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '😍OUR CHANNEL😍', url="https://t.me/latestmalayalam"
                                 )
                         ]
                     ]
